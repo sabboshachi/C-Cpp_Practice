58a6bc44281fa6be9b62af5d2dcf3285c75237fd
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-01-18 05:35:34 +0600

    name change of directory

---
+++ Cpp_Practice/02.Loops/01.forLoops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    int i;
+    for(i = 0; i <= 10; i++){
+
+        cout << i << endl;
+
+    }
+
+
+
+return 0;
+
+    
+}
