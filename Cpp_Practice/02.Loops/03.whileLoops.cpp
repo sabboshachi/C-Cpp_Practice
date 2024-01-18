8a9c1f68a595eb92653eec781001b80b1a7f9948
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-01-18 06:20:21 +0600

    while loop

---
+++ Cpp_Practice/02.Loops/03.whileLoops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+
+    int num = 1;
+
+    while(num <= 10){
+        cout << num << endl;
+        num++;
+    }
+    
+    return 0;
+}
