816ba4b4f2a70c643acee9b8d3e4b6d6aa827546
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-01-20 17:51:00 +0600

    directory updated

--- Cpp_Practice/02.Loops/01.forLoops.cpp
+++ Cpp_Practice/02.Loops/01.forLoops.cpp
@@ -4,33 +4,12 @@
 using namespace std;
 
 int main(){
-    int i, Num1, Num2;
-    
-    // Print 1 to 10 Loop
-    for(i = 0; i <= 10; i++) // initial value, condition, increment
-    {
+    int i;
+    for(i = 0; i < 10; i++){
+
         cout << i << endl;
+
     }
 
-    // Reverse Loop that print 10 to 1
-    for(i = 10; i >= 0; i--) // initial value, condition, increment
-    {
-        cout << i << endl;
-    }
-    
-    cout << "\n";
-    
-    // Taking user inputs first and last digit to create a list
-    cout << "Enter the first number of the series: ";
-    cin >> Num1;
-
-    cout << "Enter the last number of the series: ";
-    cin >> Num2;
-
-    for(i = Num1; i <= Num2; i++) // initial value, condition, increment
-    {
-        cout << i << endl;
-    }
-
-    return 0;
+return 0;    
 }
