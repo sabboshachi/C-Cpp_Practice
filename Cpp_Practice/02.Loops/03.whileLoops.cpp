cb7a1c15b924dfdb8849808b6c668fdf0c067b8a
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-01-23 23:48:41 +0600

    while loop

--- Cpp_Practice/02.Loops/03.whileLoops.cpp
+++ Cpp_Practice/02.Loops/03.whileLoops.cpp
@@ -5,12 +5,18 @@ using namespace std;
 
 int main(){
 
-    int num = 1;
+    int num = 1, num2 = 10;
 
     while(num <= 10){
         cout << num << endl;
         num++;
     }
-    
+
+
+    while(num2 >=0){
+        cout << num2 << endl;
+        num2--;
+    }
+
     return 0;
 }
