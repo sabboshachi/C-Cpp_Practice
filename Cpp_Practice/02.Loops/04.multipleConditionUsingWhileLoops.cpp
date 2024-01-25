73c90f132f81c3df56ad0f974bf30b31a804b888
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-01-26 01:17:06 +0600

    and / or operation within while loop

--- Cpp_Practice/02.Loops/04.multipleConditionUsingWhileLoops.cpp
+++ Cpp_Practice/02.Loops/04.multipleConditionUsingWhileLoops.cpp
@@ -6,9 +6,21 @@ using namespace std;
 int main(){
 
     int number = 0;
+
+    bool check = false;
+
+    cout << "How many times you want the loop to run: " ;
+    cin >> number;
+
+    while (number>=0 || check) // This condition will run only if both condition are satisfyed. But check is False so the loop won't run. 
+    {
+        cout<<number<<endl;
+        number--;
+    }
+
+    // && 'and' operator
+    // || 'or' operator
     
-
-
-
+     
     return 0;
 }
