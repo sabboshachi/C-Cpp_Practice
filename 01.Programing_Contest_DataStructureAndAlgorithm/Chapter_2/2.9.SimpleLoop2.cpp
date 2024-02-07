bf35f007ce4a17fc407dcf0467150d46eb22442f
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-02-08 01:48:15 +0600

    for and while loop understand

--- 01.Programing_Contest_DataStructureAndAlgorithm/Chapter_2/2.9.SimpleLoop2.cpp
+++ 01.Programing_Contest_DataStructureAndAlgorithm/Chapter_2/2.9.SimpleLoop2.cpp
@@ -7,35 +7,34 @@ using namespace std;
 
 int main(){
 
-    // counts how many 2 devides 100
-    int x = 100, count = 0;
+    // counts how many 2 devides 100 ######################
+    int x = 1024, count = 0;
     while(x % 2 == 0)
     {
         // we can write x /= 2 
         x = x / 2;
         count ++;
     }
+    cout << count << endl;
 
-    // find out the highsat number which is power of 2 and less than 1000
-    int a = 1, sum=1;
+    // find out the highsat number which is power of 2 and less than 1000  
+    int a = 1;
     while (a * 2 < 1000)
     {
-        sum = a * 2;
-        a = a * 2;
+        a = a * 2; 
+        // at the last point when a=512 it will back to the loop condition then check if 512*2 < 1000 
+        // and so the finla value of a will be 512
     }
-    cout << "The number is: " << sum << endl;
+    cout << "The number is: " << a << endl;
 
-    // same thing using for loop. Note there is a semicolon after the for loop. at the end of loop, you will find desired value in x
-    for (x=1; x*x<1000; x=x*2){
-        cout << "The number is: " << x << endl;
+
+    // same thing using for loop. Note there is a semicolon after the for loop. at the end of loop, you will find desired value in x. ######################
+    int i;
+
+    for(i=1;i*2<1000;i=i*2){
+        // cout << i << endl;
     }
-    
+    cout << "Final value of a: " << i << endl;
 
-
-
-
-
-
-
-    return 0;
+   return 0;
 }
