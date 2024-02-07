3cb48d747ed39b9bca1083447f82553613950638
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-02-08 02:54:35 +0600

    different loop system

---
+++ 01.Programing_Contest_DataStructureAndAlgorithm/Chapter_2/2.11.SimpleLoop4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // veru important. 
+    // initialize variable 
+    int sum = 0, i, j, n;
+
+    // i is the upper bound
+    for(i = 1; i<=n; i++){
+        // we will dum up the numbers from 1 to i
+        for (j = 1; j<=1; j++){
+            sum +=1;
+        }
+    }
+    return 0;
+}
+
+
