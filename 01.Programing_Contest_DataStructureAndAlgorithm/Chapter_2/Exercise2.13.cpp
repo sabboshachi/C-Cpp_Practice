452d283c3a5dc9f9354c6cd706e548950a2f9fe5
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-02-23 23:56:49 +0600

    sum of square series

---
+++ 01.Programing_Contest_DataStructureAndAlgorithm/Chapter_2/Exercise2.13.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+    int n, i, sum=0;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    for(int i=1; i<=n; i++){
+        i=i*i;
+        sum = sum+i;
+    }
+    cout << sum;
+    return 0;
+}
