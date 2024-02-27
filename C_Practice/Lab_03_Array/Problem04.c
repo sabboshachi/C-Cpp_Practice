134c5ac8c01248d0fb5502d8fd96e07f2cfa85fd
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-02-27 16:25:56 +0600

    array descending order
    Write a C program to sort elements of an array in descending order.

---
+++ C_Practice/Lab_03_Array/Problem04.c
@@ -0,0 +1,38 @@
+// Problem 04: Write a C program to sort elements of an array in descending order.
+
+#include <stdio.h>
+
+int main() {
+    int array[100], size, i, j, temp;
+
+    // Input the size of the array
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    // Input the elements of the array
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    // Sort the elements of the array in descending order
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                // Swap array[j] and array[j + 1]
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    // Print the sorted array in descending order
+    printf("Sorted array in descending order: ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
