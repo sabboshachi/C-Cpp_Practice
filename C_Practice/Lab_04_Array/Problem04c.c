f6d5e6811cd77bed52503a7224cd41bec85888b5
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-02-27 16:37:35 +0600

    update

--- C_Practice/Lab_04_Array/Problem04c.c
+++ C_Practice/Lab_04_Array/Problem04c.c
@@ -1,8 +1,6 @@
 // Problem 04c: The annual examination result of 20 students is tabulated as follow:
 // Roll no.              Sub1                 Sub2                      Sub3
 // Write a C program to read the data and determine the followings:
-// a)	Total marks obtained by each student
-// b)	The highest mark in each subject and the roll no. of the student who obtained it.
 // c)	The student who obtained the highest total marks.
 
 #include <stdio.h>
