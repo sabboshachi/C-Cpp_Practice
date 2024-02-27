78a8cefa50e6e6ce954f4e9bafb1144288c86075
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-02-27 14:37:47 +0600

    sum of array
    Write a C program to find the sum of marks of n students using array.

---
+++ C_Practice/Lab_02_Array/Problem01.c
@@ -0,0 +1,17 @@
+// Problem 01: Write a C program to find the sum of marks of n students using array.
+
+#include<stdio.h>
+
+int main(){
+
+    int marks[20],i,n,sum=0;
+    printf("Enter number of sutdets: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("Enter marks of student %d : ", i+1);
+        scanf("%d",&marks[i]); 
+        sum = sum + marks[i];
+    }
+    printf("Sum = %d",sum);
+    return 0;
+}
