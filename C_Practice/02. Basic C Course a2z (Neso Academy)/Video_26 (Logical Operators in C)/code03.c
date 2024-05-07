5fc509d4c5f4fb350c68a446e117fea38a9cb562
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-08 02:48:39 +0600

    conditional NOT

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_26 (Logical Operators in C)/code03.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a = 5;
+    if (!(a==6)){
+        printf("The condition returns TRUE, that is why the printf fucntion is working.");
+    }
+
+    return 0;
+}
