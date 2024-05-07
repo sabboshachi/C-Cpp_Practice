ac100666ee537ca52515e649d545653c3d9ce7c5
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-07 18:00:26 +0600

    bitwise left shift

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_28 (Bitwise Operators in C - Part 02)/code01.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    char var =3;
+    printf("%d", var << 1);
+    
+    return 0;
+}
+
+// 0000 0011 = 3
+// 1 left shift
+// 0000 0110 = 6
+// and so the output will be 6
