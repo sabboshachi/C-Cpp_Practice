6a45140fac105ab62d2857a1fe10fc9c2589557d
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-08 04:04:59 +0600

    Arithmetic Operators

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_22 (Arithmatic Operators in C)/code01.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a = 2, b = 3, c = 4, d = 5;
+
+    printf("a*b/c = %d\n", a*b/c);
+    printf("a+b-c = %d\n", a+b-c);
+    printf("a+b*d-c%%d = %d\n", a+b*d-c%d);
+
+    return 0;
+}
