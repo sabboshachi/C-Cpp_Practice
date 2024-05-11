3c9e1db6e7ee6af2ccaf0210fff5a01c1514d7e7
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-11 23:52:23 +0600

    basic input function

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_05 (Basic Output Function - printf)/code01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int var = 2;
+    int var1 = 25;
+    int var2 = 3;
+
+    printf("Sabboshachi\n");
+
+    printf("%d\n", var);
+
+    printf("%d %d", var1, var2);
+
+    return 0;
+}
