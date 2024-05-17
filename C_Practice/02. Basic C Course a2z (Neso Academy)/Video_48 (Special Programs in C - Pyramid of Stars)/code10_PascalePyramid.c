1a3b70afd6b6affee80ae8db17c2fbd369aef730
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-17 08:09:56 +0600

    pascal triangle _ NOT DONE

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_48 (Special Programs in C - Pyramid of Stars)/code10_PascalePyramid.c
@@ -0,0 +1,18 @@
+// #include<stdio.h>
+// int main(){
+//     int n=5, j, i;
+//     for (i=1; i<=n; i++){
+//         for(j=1; j <=2*n-1; j++){
+//             if(j>=n-(i-1) && j<=n+(i-1)){
+//                 printf("%d",j);
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// Need more explanation
