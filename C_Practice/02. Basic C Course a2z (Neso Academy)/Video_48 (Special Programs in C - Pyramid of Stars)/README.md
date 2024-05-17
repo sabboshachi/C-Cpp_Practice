479be50b5304e5335c506b5fb1f3e358d304c05d
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-17 06:45:50 +0600

    star pyramid

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_48 (Special Programs in C - Pyramid of Stars)/README.md
@@ -0,0 +1,8 @@
+# Video_48 (Special Programs in C - Pyramid of Stars)
+
+## [Video Link](https://www.youtube.com/watch?v=KdM6OrvcjPI&list=PLBlnK6fEyqRhX6r2uhhlubuF5QextdCSM&index=49&ab_channel=NesoAcademy)
+
+## Pyramid Concept
+## ![Alt text](pyramidconcept.jpg)
+
+
---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_48 (Special Programs in C - Pyramid of Stars)/code03.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n=10, j, i;
+    for (i=1; i<=n; i++){
+        for(j=1; j <=2*n-1; j++){
+            if(j>=n-(i-1) && j<=n+(i-1)){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
