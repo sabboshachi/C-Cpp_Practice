771a4f1fa4a9598aaafb64d0e4d9bcf05cd2bd93
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-18 03:01:54 +0600

    problem solve

---
+++ C_Practice/Extra/ProblemSolve02.c
@@ -0,0 +1,10 @@
+// Q2: What is the output of this code?
+#include <stdio.h>
+int main() {
+    int n=20, m=2;
+    printf("%d", printf("%d%d",n,m)); // Here the printf function counts how many 
+    // digits are there in the given number or string
+    return 0;
+}
+// Here we gave 20 and 2 which together has 3 digits 
+// so the output will be 2023
