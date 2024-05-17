1894922ed017916bda89d9cfa9976b6277173c4a
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-18 03:01:44 +0600

    question solve

---
+++ C_Practice/Extra/ProblemSolve01.c
@@ -0,0 +1,10 @@
+// Q1: What is the output of this code?
+#include <stdio.h>
+int main() {
+    int n, m;
+    printf("%d", scanf("%d %d",&n,&m)); // Here the printf function counts how many 
+    // integers are given by the user. The nunber of the inputs will be the answer.
+    return 0;
+}
+// Here we gave two inputs so the output will be 2
+
