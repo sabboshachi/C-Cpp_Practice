2b016089a68ed3da160a0602301013a4abb66523
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-18 10:24:43 +0600

    Update code01_ArmstrongNumber.c

--- C_Practice/02. Basic C Course a2z (Neso Academy)/Video_50 (Special Programs in C - Check if the Number is Armstrong Number)/code01_ArmstrongNumber.c
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_50 (Special Programs in C - Check if the Number is Armstrong Number)/code01_ArmstrongNumber.c
@@ -2,7 +2,7 @@
 int main(){
     int number = 371, count = 0, q, cnt, results = 0, mul = 1, reminder;
 
-    // Step 01: Find out how many digits are there in your number
+    // Step 01: Find out how many digits are there in your number ####################
     q = number;
     while (q != 0)
     {
@@ -12,7 +12,7 @@ int main(){
     //printf("Number of Digits = %d\n", count);
     //printf("q = %d\n", q);
 
-    // Multiply each digits n times and add them
+    // Multiply each digits n times and add them ####################
     cnt = count;
     q = number;
     while(q != 0){
@@ -28,7 +28,7 @@ int main(){
     } 
     //printf("Resutls: %d\n", results);
     
-    // Check the number with results
+    // Check the number with results ####################
     if (results == number)
         printf("The given number is an Armstrong Number.");
     else 
