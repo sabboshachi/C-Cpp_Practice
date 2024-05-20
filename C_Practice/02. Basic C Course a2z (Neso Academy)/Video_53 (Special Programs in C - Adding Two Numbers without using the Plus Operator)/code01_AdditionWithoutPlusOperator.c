589ac498e7b12b01250ec0dfcbf6cfeb0a6807c5
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-20 10:00:21 +0600

    sum without plus operator

--- C_Practice/02. Basic C Course a2z (Neso Academy)/Video_53 (Special Programs in C - Adding Two Numbers without using the Plus Operator)/code01_AdditionWithoutPlusOperator.c
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_53 (Special Programs in C - Adding Two Numbers without using the Plus Operator)/code01_AdditionWithoutPlusOperator.c
@@ -2,9 +2,11 @@
 #include<math.h>
 
 int main(){
-
-
-
-    
+    int x = 3, y = 5;
+    while (y != 0){
+        x++;
+        y--;
+    }
+    printf("The sum of the given numbers are: %d",x);
     return 0;
 }
