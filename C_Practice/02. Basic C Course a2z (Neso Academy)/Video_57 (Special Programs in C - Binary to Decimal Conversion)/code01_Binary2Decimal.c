7f1d031c124746b9085a0b0748de1d39129e863d
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-05-20 17:27:29 +0600

    binary to decimal converter

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_57 (Special Programs in C - Binary to Decimal Conversion)/code01_Binary2Decimal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    int binary = 1000000000, weight = 1, decimal = 0, rem;
+
+    // printf("Enter a binary number:");
+    // scanf("%d", &binary);
+
+    while(binary != 0){
+        rem = binary % 10; // taking the list digit as reminder
+        decimal = decimal + rem*weight; // digit*weight 
+        weight = weight*2; // increase weight by 2x as binary contain 2
+        binary = binary / 10; 
+    }
+    printf("%d", decimal);
+    return 0;
+}
