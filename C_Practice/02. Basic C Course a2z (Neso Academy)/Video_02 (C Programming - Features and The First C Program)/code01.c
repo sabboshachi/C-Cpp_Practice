a733658c8461ec6a55d957ea89e05a881f470dbc
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-06-01 10:09:51 +0600

    update lecture note

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_02 (C Programming - Features and The First C Program)/code01.c
@@ -0,0 +1,5 @@
+#include<stdio.h> // Preporcessor directive (standard input and output)(.h = header file)
+int main(){ // main funciton
+    printf("Hi, I am Sabboshachi!"); // printf function from stdio.h
+    return 0;
+}
