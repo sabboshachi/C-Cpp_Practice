b5a6a72ee549b821fc5ec4cf30fb1e26f69ff04e
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-06-13 15:54:01 +0600

    Update code02.c

--- C_Practice/02. Basic C Course a2z (Neso Academy)/Video_68 (Static Functions in C)/code02.c
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_68 (Static Functions in C)/code02.c
@@ -1,3 +1,11 @@
 int fun(int x, int y){
     return(x+y);
 }
+
+// but here if we add 'static' before the fun()
+// it won't be accessable by the main function
+// the function will usable only inside the code
+
+static fun(int x, int y){
+    return(x+y);
+}
