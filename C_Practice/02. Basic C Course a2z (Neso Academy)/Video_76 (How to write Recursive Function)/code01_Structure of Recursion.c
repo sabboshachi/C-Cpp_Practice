ddbe7e0093133478340bb4b3611a5a8786241d97
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-06-20 23:43:33 +0600

    structure of recursive program

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_76 (How to write Recursive Function)/code01_Structure of Recursion.c
@@ -0,0 +1,19 @@
+
+// Step_01: Divide problems into smaller problems 
+// Step_02: Specify the base conditon to stop the recursion
+
+fact(){
+    if(){
+
+        // Base Case (2)
+
+    }
+    else{
+
+        // Recursive Procedure (1)
+
+    }
+}
+
+// Here we first need to specify our recursive procedure then move towards base case.
+// Example on code02.c
