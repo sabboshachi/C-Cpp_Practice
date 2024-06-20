a25ca4eebd3a9104f9237ae6e43ef9b627f16dcd
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-06-20 23:34:23 +0600

    recursion in C

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_75 (Recursion in C)/code01.c
@@ -0,0 +1,7 @@
+// Recursion is a process in which a function calls itself directly or indirectly.
+
+int fun(){
+    
+    fun(); // Calling the function inside itself
+    
+}
---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_75 (Recursion in C)/code02.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fun(int n){
+        if(n == 1){
+            return 1;
+        }
+        else{
+            return 1 + fun(n-1); // Here we are calling the function and also reducing its value and adding one to it
+        }
+}
+int main(){
+    int n = 3;
+    printf("%d", fun(n)); 
+    return 0;
+}
+
+// Here we need to understand the flow of function 
+// fun(3) ---> return 1 + fun(2) ---> return 1 + fun(1) ---> return 1
+//                                                              |
+//  <--               <--                   <--              <--
