a23967b90555d6785f1db7101c65eff80d9503b2
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-06-23 00:05:13 +0600

    Update code03_NonTail_Recursion.c

--- C_Practice/02. Basic C Course a2z (Neso Academy)/Video_78 (Types of Recursion - Part 02 - Tails and NonTail Recursion/code03_NonTail_Recursion.c
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_78 (Types of Recursion - Part 02 - Tails and NonTail Recursion/code03_NonTail_Recursion.c
@@ -12,3 +12,9 @@ int main(){
     printf("%d", fun(8));
     return 0;
 }
+
+// fun(8) --> return 1 + fun(4) --> return 1 + fun(2)  --> return 1 + fun(1) --> fun(1) = 0
+//                                                                                     |
+// printf(3)        (1 + 2)     <---       (1 + 1)    <---       (1 + 0)           <----
+
+// Output: 3
