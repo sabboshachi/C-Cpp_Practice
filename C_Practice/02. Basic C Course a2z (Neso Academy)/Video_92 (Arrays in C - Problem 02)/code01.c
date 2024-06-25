d66c95b0f8f67799d5da806c5c0f63c342af06fd
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-06-25 13:47:43 +0600

    array problem solved
    check if digits in a number appears more than once

---
+++ C_Practice/02. Basic C Course a2z (Neso Academy)/Video_92 (Arrays in C - Problem 02)/code01.c
@@ -0,0 +1,29 @@
+// Write a program to check whether any of the digits in a number appears more than once.
+// Example: 
+// Input: 67827
+// Output: Yes
+
+#include<stdio.h>
+int main(){
+    int number = 67827, x, reminder = 0, count = 0;
+    
+    int a[10] = {0};
+    // here we will target the index number, not the value
+    // after getting each remider replace the value of the array[reminder] = 1
+
+    while (number>0)
+    {
+        reminder = number % 10;
+        if(a[reminder] == 1){ // if we previously got 1 on that index, break the loop
+            break;
+        }
+        a[reminder] = 1;
+        number = number / 10;
+    }
+    if(number > 0){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+}
